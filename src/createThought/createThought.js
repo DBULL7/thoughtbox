36126da7594a1ec552483cc791c925e6813bd9c2
Devon Bull <dbull@live.com>
2017-05-04 21:14:25 -0600

    code from in class code along

--- src/createThought/createThought.js
+++ src/createThought/createThought.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
 
 export default class CreateThought extends Component {
-  render() {
-    return(
-      <div>CreateThought</div>
-    );
-  }
+  constructor() {
+   super()
+   this.state = {
+     title: "",
+     body: ""
+   }
+ }
+
+submitIdea() {
+  console.log('yay');
+  this.props.createThought(this.state)
+  this.setState({title: '', body: ''})
+}
+
+ render() {
+   return(
+     <div>CreateThought
+       <input value={this.state.title} onChange={ (e) => this.setState({ title: e.target.value }) } />
+       <input value={this.state.body} onChange={ (e) => this.setState({ body: e.target.value }) }/>
+       <button onClick={() => this.submitIdea()}>Submit</button>
+     </div>
+   );
+ }
 
 }
